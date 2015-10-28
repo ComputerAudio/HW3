b30156d0c35e7ffc28f74415ab18b6ce0b362444
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-10-27 22:19:23 -0400

    Added a car spawner

---
+++ HW3/Assets/Scripts/CarDrive.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarDrive : MonoBehaviour {
+    public float speed;
+
+    Vector3 origin;
+
+    void Start()
+    {
+        origin = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        checkDelete();
+    }
+
+    void checkDelete()
+    {
+        float mag = (transform.position - origin).magnitude;
+        if (mag > 100)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ HW3/Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSpawner : MonoBehaviour {
+    public Transform[] cars;
+    public float minSpawnTime = .5f;
+    public float maxSpawnTime = 1f;
+
+    private float timer;
+
+    void Start()
+    {
+        resetTimer();
+    }
+
+	void Update()
+    {
+        timer = Mathf.MoveTowards(timer, 0, Time.deltaTime);
+        if (timer <= 0)
+        {
+            createCar();
+            resetTimer();
+        }
+    }
+
+    void createCar()
+    {
+        GameObject obj = (GameObject)Instantiate(cars[Random.Range(0, cars.Length)].gameObject, this.transform.position, new Quaternion());
+        obj.transform.rotation = this.transform.localRotation;
+    }
+
+    void resetTimer()
+    {
+        timer = Random.Range(minSpawnTime, maxSpawnTime);
+    }
+}
