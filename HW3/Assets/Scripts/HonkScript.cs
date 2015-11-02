edd82a6fa09a50a98ed49504c1c08e650391d743
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-02 17:50:22 -0500

    Car Honk added

---
+++ HW3/Assets/Scripts/HonkScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HonkScript : MonoBehaviour {
+    AudioSource aSource;
+
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            aSource.Stop();
+            aSource.pitch = Random.Range(.7f, 1.3f);
+            aSource.Play();
+        }
+    }
+}
