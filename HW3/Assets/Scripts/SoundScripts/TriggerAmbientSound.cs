6fc6843d4f6bb2ff11ae08c2ee770299df0a6895
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-02 18:21:57 -0500

    Ambient Triggers

---
+++ HW3/Assets/Scripts/SoundScripts/TriggerAmbientSound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAmbientSound : MonoBehaviour {
+    public AudioClip aClip;
+    public AudioSource ambientSound;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            if (ambientSound.clip.name == aClip.name)
+            {
+                return;
+            }
+            ambientSound.Stop();
+            ambientSound.clip = aClip;
+            ambientSound.Play();
+        }
+    }
+}
