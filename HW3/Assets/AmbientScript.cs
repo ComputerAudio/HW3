796ccb49984820908b95e88b3a105deaf8ebf01b
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-02 23:26:29 -0500

    Outdoor ambient sounds added

---
+++ HW3/Assets/AmbientScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientScript : MonoBehaviour {
+    public float timeStep = .5f;
+    AudioSource aSource;
+    AudioClip pendingClip;
+    bool changeClip;
+    float maxVolume;
+
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+        maxVolume = aSource.volume;
+    }
+
+    void Update()
+    {
+        if (!changeClip)
+        {
+            aSource.volume = Mathf.MoveTowards(aSource.volume, maxVolume, Time.deltaTime * timeStep);
+        }
+        else
+        {
+            aSource.volume = Mathf.MoveTowards(aSource.volume, 0, Time.deltaTime * timeStep);
+            if (aSource.volume <= 0)
+            {
+                changeClip = true;
+                aSource.Stop();
+                aSource.clip = pendingClip;
+                aSource.Play();
+            }
+        }
+    }
+
+    public void changeAmbientSound(AudioClip aClip)
+    {
+        if (aClip.name == aSource.clip.name)
+        {
+            return;
+        }
+        pendingClip = aClip;
+        changeClip = true;
+
+    }
+}
--- HW3/Assets/Scripts/SoundScripts/TriggerAmbientSound.cs
+++ HW3/Assets/Scripts/SoundScripts/TriggerAmbientSound.cs
@@ -3,19 +3,13 @@ using System.Collections;
 
 public class TriggerAmbientSound : MonoBehaviour {
     public AudioClip aClip;
-    public AudioSource ambientSound;
+    public AmbientScript ambientSound;
 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "Player")
         {
-            if (ambientSound.clip.name == aClip.name)
-            {
-                return;
-            }
-            ambientSound.Stop();
-            ambientSound.clip = aClip;
-            ambientSound.Play();
+            ambientSound.changeAmbientSound(aClip);
         }
     }
 }
