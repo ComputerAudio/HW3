a6dd488cb67fbb9555df300e4a18c1c9eabe971f
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-03 02:05:53 -0500

    IHaveMoney added

---
+++ HW3/Assets/MoveSide.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSide : MonoBehaviour {
+    public Transform[] destination;
+    public float speed;
+
+    Vector3[] destinationVector;
+    int current;
+
+    void Start()
+    {
+        destinationVector = new Vector3[destination.Length];
+        int i = 0;
+        foreach (Transform t in destination)
+        {
+            destinationVector[i] = t.position;
+            i++;
+        }
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.LerpUnclamped(transform.position, destinationVector[current], Time.deltaTime * speed);
+        if (Mathf.Abs((transform.position - destinationVector[current]).magnitude) < 1f)
+        {
+            changeCurrentDestination();
+        }
+    }
+
+    void changeCurrentDestination()
+    {
+        current++;
+        current = current % destination.Length;
+    }
+    
+}
