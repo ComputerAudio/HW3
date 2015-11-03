93b9e5fa76e28c6a3b123fe6206979cd8d922a46
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-03 01:41:22 -0500

    Why Hello there added to hostess

---
+++ HW3/Assets/TriggerSound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerSound : MonoBehaviour {
+    public bool playOnce;
+    public AudioClip aClip;
+
+    bool soundPlayed;
+    AudioSource aSource;
+
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            if (playOnce && soundPlayed)
+            {
+                return;
+            }
+            aSource.Stop();
+            aSource.clip = aClip;
+            aSource.Play();
+            soundPlayed = true;
+        }
+    }
+}
